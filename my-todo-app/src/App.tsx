eeab4487d9072c5bd9cd20c8ca9696cfe1808325
jooooonis <xowns3213@naver.com>
2022-08-14 19:29:05 +0900

    feat : theme 설정 style 상수를 사용하기 위해 theme을 생성하고 전역으로 컴포넌트에 제공합니다.

--- my-todo-app/src/App.tsx
+++ my-todo-app/src/App.tsx
@@ -3,12 +3,27 @@ import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import SignInPage from './pages/SignInPage';
 import HomePage from './pages/HomePage';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3894D6',
+    },
+    secondary: {
+      main: '#11cb5f',
+    },
+  },
+});
+
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />}></Route>
-      <Route path='/signin' element={<SignInPage />}></Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/signin' element={<SignInPage />}></Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
