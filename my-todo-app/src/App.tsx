5d20d85cadb3633f88a12187b7889cd39c954968
jooooonis <xowns3213@naver.com>
2022-08-14 18:49:36 +0900

    feat : Router 설정 App.tsx에서 Router를 관리합니다.

--- my-todo-app/src/App.tsx
+++ my-todo-app/src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
+import React from 'react';
+import SignInPage from './pages/SignInPage';
+import HomePage from './pages/HomePage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+      <Route path='/signin' element={<SignInPage />}></Route>
+    </Routes>
   );
 }
 
