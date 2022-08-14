a755eef0ec45263c9dc86808d3a53c86b209eaaf
jooooonis <xowns3213@naver.com>
2022-08-15 00:31:43 +0900

    feat : 홈 하단에 버튼추가 Avatar를 사용하여 버튼을 만들었습니다.

--- my-todo-app/src/components/Home.tsx
+++ my-todo-app/src/components/Home.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import Header from './common/Header';
-import { Box } from '@mui/material';
-
+import { Box, Avatar } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import Todos from './Todos';
+
 function Home() {
   return (
     <Box>
       <Header />
       <Todos />
+      <Avatar
+        onClick={() => console.log('clicks')}
+        sx={{
+          bgcolor: 'white',
+          position: 'fixed',
+          right: '30px',
+          bottom: '60px',
+          width: 55,
+          height: 55,
+          '&:hover': {
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}>
+        <AddIcon
+          sx={{
+            color: 'primary.main',
+            width: 30,
+            height: 30,
+          }}
+        />
+      </Avatar>
     </Box>
   );
 }
